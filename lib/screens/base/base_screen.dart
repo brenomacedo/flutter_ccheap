cbcec4f551cf6dddff5e211d0ffc870e3d151287
brenosekiro <b88660501@gmail.com>
2021-03-17 18:46:20 -0300

    create_screen

--- lib/screens/base/base_screen.dart
+++ lib/screens/base/base_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get_it/get_it.dart';
 import 'package:mobx/mobx.dart';
+import 'package:xlo_mobx/screens/create/create_screen.dart';
 import 'package:xlo_mobx/screens/home/home_screen.dart';
 import 'package:xlo_mobx/stores/page_store.dart';
 
@@ -32,7 +33,7 @@ class _BaseScreenState extends State<BaseScreen> {
         physics: NeverScrollableScrollPhysics(),
         children: [
           HomeScreen(),
-          Container(color: Colors.greenAccent),
+          CreateScreen(),
           Container(color: Colors.purple),
           Container(color: Colors.red),
           Container(color: Colors.yellow),
---
+++ lib/screens/create/create_screen.dart
@@ -0,0 +1,65 @@
+import 'package:brasil_fields/brasil_fields.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:xlo_mobx/components/custom_drawer/custom_drawer.dart';
+
+class CreateScreen extends StatelessWidget {
+
+  final labelStyle = TextStyle(
+    fontWeight: FontWeight.w800,
+    color: Colors.grey,
+    fontSize: 18
+  );
+
+  final contentPadding = EdgeInsets.fromLTRB(16, 10, 12, 10);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Criar anúncio'),
+        centerTitle: true,
+      ),
+      drawer: CustomDrawer(),
+      body: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(16))
+        ),
+        margin: EdgeInsets.symmetric(horizontal: 16),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Título*',
+                labelStyle: labelStyle,
+                contentPadding: contentPadding
+              ),
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Descrição*',
+                labelStyle: labelStyle,
+                contentPadding: contentPadding
+              ),
+              maxLines: null,
+            ),
+            TextFormField(
+              decoration: InputDecoration(
+                labelText: 'Preço*',
+                labelStyle: labelStyle,
+                contentPadding: contentPadding,
+                prefixText: 'R\$'
+              ),
+              keyboardType: TextInputType.number,
+              inputFormatters: [
+                FilteringTextInputFormatter.digitsOnly,
+                RealInputFormatter(centavos: true)
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
